c41dfae3b1cdfb0445e40d208243ff9d8d71cbaf
Marc Abonce Seguin <marc-abonce@mailbox.org>
2019-07-29 21:25:05 -0700

    [mod] separate index and search routes
    This makes it easier to separately handle search and index requests
    from a web server or from a reverse proxy.
    
    If a request to index contains a query, a permanent redirect HTTP response
    is returned. This should give some level of backwards compatibility
    for users that have set a searx instance in their browser's search bar.

--- tests/unit/test_webapp.py
+++ tests/unit/test_webapp.py
@@ -76,6 +76,11 @@ class ViewsTestCase(SearxTestCase):
 
     def test_index_html(self):
         result = self.app.post('/', data={'q': 'test'})
+        self.assertEqual(result.status_code, 308)
+
+    def test_search_html(self):
+        result = self.app.post('/search', data={'q': 'test'})
+
         self.assertIn(
             b'<h3 class="result_title"><img width="14" height="14" class="favicon" src="/static/themes/legacy/img/icons/icon_youtube.ico" alt="youtube" /><a href="http://second.test.xyz" rel="noreferrer">Second <span class="highlight">Test</span></a></h3>',  # noqa
             result.data
@@ -87,7 +92,10 @@ class ViewsTestCase(SearxTestCase):
 
     def test_index_json(self):
         result = self.app.post('/', data={'q': 'test', 'format': 'json'})
+        self.assertEqual(result.status_code, 308)
 
+    def test_search_json(self):
+        result = self.app.post('/search', data={'q': 'test', 'format': 'json'})
         result_dict = json.loads(result.data.decode('utf-8'))
 
         self.assertEqual('test', result_dict['query'])
@@ -97,6 +105,10 @@ class ViewsTestCase(SearxTestCase):
 
     def test_index_csv(self):
         result = self.app.post('/', data={'q': 'test', 'format': 'csv'})
+        self.assertEqual(result.status_code, 308)
+
+    def test_search_csv(self):
+        result = self.app.post('/search', data={'q': 'test', 'format': 'csv'})
 
         self.assertEqual(
             b'title,url,content,host,engine,score,type\r\n'
@@ -107,6 +119,10 @@ class ViewsTestCase(SearxTestCase):
 
     def test_index_rss(self):
         result = self.app.post('/', data={'q': 'test', 'format': 'rss'})
+        self.assertEqual(result.status_code, 308)
+
+    def test_index_rss(self):
+        result = self.app.post('/search', data={'q': 'test', 'format': 'rss'})
 
         self.assertIn(
             b'<description>Search results for "test" - searx</description>',
